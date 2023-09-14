b70d16d93e9e8a168f07f857361c03137ce368f2
Tina Luu <tina.luu@mrjaffesclass.com>
2023-09-14 11:33:41 -0700

    Operators code added

---
+++ Operators/Operators.java
@@ -0,0 +1,14 @@
+public class Operators
+{
+    public static void main(String[] args) 
+    {
+        int i = 10;
+        int f = 1;
+        f += i;
+        boolean flag = true;
+        flag = !flag;
+        System.out.println(++i);
+        System.out.println(f);
+        System.out.println(flag);
+    }
+}
